2076bf9149ad0293ce8e7a69040ad6834da788fa
Justin Peisker <justinpeisker@gmail.com>
2021-08-13 10:38:09 -0700

    questions and css done

--- README.md
+++ README.md
@@ -23,10 +23,24 @@ Commit your code regularly and meaningfully.
 Please answer the following questions below, you may edit the readme file to include your answers below the question.
 
 1. How would you describe acessibility on the web to someone new to programming?
+
+    Assessibility is the concept that anyone, regardless of their impairment or disability, can have full access to the web and all it has to offer.
+
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+
+    Using semantic HTML to build your website.  Providing a descriptive alt attribute on images.  Desiging with sufficient foreground and background contrast.
+
 3. How would you explain the concept of a variable to someone new to programming?
+
+    A variable is a storage container for data that you assign to it.
+
 4. What is the purpose of using functions in code?
+
+    The purpose of functions is to allow us to repeat a multi-lined section of code with just the single line of function code. 
+     
 5. How do you access a key inside of an object inside of an array?
+
+    return array[index].key;
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
--- index.js
+++ index.js
@@ -208,16 +208,18 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0]);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+artists[8].name = 'Vincent Van Gogh';
 
+console.log(artists[8].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,11 +230,11 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  return `the artist at index ${array[index].id} is ${array[index].name}`;
 }
 
-
+console.log('TASK 3', getArtistByIndex(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use get20s to do the following: 
@@ -243,9 +245,18 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  const newArray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i].years.split() >= '1900' && array[i].years.split() <= '2000'){
+       newArray.push(array[i]);
+    }
+  }
+  return newArray;
 }
+
+console.log(get20s(artists));
 
 
 
@@ -258,10 +269,11 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  
+  // return array[index].slice(2,2);
 }
-
+console.log(removeArtist(artists[0]));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -279,11 +291,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  return array.push({'id''name''years''genre''nationality''bio'});
 }
 
-
+console.log(addArtist(20,'justin peisker', '1985 - 2021','web design', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore cupiditate amet ipsam exercitationem recusandae distinctio non, inventore ut eos? Ipsa!'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
